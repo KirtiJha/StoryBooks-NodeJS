b13761e1b77a01cc8a3f1f439609b133900b6faf
Kirti Jha <kirtijha@in.ibm.com>
2019-01-08 19:59:26 +0530

    Setup server

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
-console.log('Hello World');
+const express = require('express');
+const mongoose = require('mongoose');
+
+const app = express();
+
+const port = process.env.PORT || 5000;
+
+app.get('/', (req, res) => {
+  res.send('INDEX');
+});
+
+app.listen(port, () => {
+  console.log(`Server started on port ${port}`);
+});
