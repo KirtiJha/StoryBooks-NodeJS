4a47feec42fbb535e3d99dd0e477074e0311c483
Kirti Jha <kirtijha@in.ibm.com>
2019-01-09 17:07:29 +0530

    Finish Google OAuth

--- app.js
+++ app.js
@@ -1,13 +1,31 @@
 const express = require('express');
 const exphbs  = require('express-handlebars');
+const session = require('express-session');
+const cookieParser = require('cookie-parser')
 const mongoose = require('mongoose');
 const passport = require('passport');
+
+// Load User model
+require('./models/User');
 
 // Pasport Config
 require('./config/passport')(passport)
 
 // Load Routes
 const auth = require('./routes/auth');
+
+// Load Keys
+const keys = require('./config/keys');
+
+// Map global promise - get rid of warning
+mongoose.Promise = global.Promise;
+
+// Mongoose connect
+mongoose.connect(keys.mongoURI, {
+  useNewUrlParser: true
+})
+.then(() => console.log('MongoDB Connected....'))
+.catch(err => console.log(err));
 
 const app = express();
 
@@ -17,7 +35,25 @@ app.engine('handlebars', exphbs({
 }));
 app.set('view engine', 'handlebars');
 
-const port = process.env.PORT || 5000;
+// Express Session Middleware
+app.use(session({
+  secret: 'secret',
+  resave: false,
+  saveUninitialized: false
+}));
+
+// Passport Middleware (Should be after Express session middleware)
+app.use(passport.initialize());
+app.use(passport.session());
+
+// Cookie-Parser Middleware
+app.use(cookieParser());
+
+// Global Variables
+app.use(function(req, res, next) {
+  res.locals.user = req.user || null;
+  next();
+});
 
 app.get('/', (req, res) => {
   res.render('index');
@@ -26,6 +62,7 @@ app.get('/', (req, res) => {
 // Use Routes
 app.use('/auth', auth);
 
+const port = process.env.PORT || 5000;
 app.listen(port, () => {
   console.log(`Server started on port ${port}`);
 });
