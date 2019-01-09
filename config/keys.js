50e2e957763037aa36c8fa43fc09c210cc9190c3
Kirti Jha <kirtijha@in.ibm.com>
2019-01-09 13:19:19 +0530

    Setup Google OAuth

---
+++ config/keys.js
@@ -0,0 +1,5 @@
+module.exports = {
+  mongoURI: 'mongodb://kirti:bacardi1986@ds161487.mlab.com:61487/heroku_qf7fphz9',
+  googleClientID:'180474729333-u4n3je0ct0atrlucbej2gk29km72cg9c.apps.googleusercontent.com',
+  googleClientSecret: 'jivhBpeLZc63HIdw0h853uQB'
+}
