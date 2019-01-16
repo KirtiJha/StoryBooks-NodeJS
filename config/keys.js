8ec8e6588240a0d1a2e20f4ad1f08bbe9376a96f
Kirti Jha <kirtijha@in.ibm.com>
2019-01-16 18:06:34 +0530

    Setup prod database and Env variables in Heroku

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,5 @@
-module.exports = {
-  mongoURI: 'mongodb://kirti:bacardi1986@ds161487.mlab.com:61487/heroku_qf7fphz9',
-  googleClientID:'180474729333-u4n3je0ct0atrlucbej2gk29km72cg9c.apps.googleusercontent.com',
-  googleClientSecret: 'jivhBpeLZc63HIdw0h853uQB'
+if(process.env.NODE_ENV === 'production') {
+  module.exports = require('./keys_prod');
+} else {
+  module.exports = require('./keys_dev');
 }
---
+++ config/keys_prod.js
@@ -0,0 +1,5 @@
+module.exports = {
+  mongoURI: process.env.MONGOLAB_COPPER_URI,
+  googleClientID: process.env.GOOGLE_CLIENT_ID,
+  googleClientSecret: process.env.GOOGLE_CLIENT_SECRET
+}
