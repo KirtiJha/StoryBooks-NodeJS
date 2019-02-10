7d88eaf47884f972347193c7554676a97a4e5247
Kirti N. Jha <kirtijha@in.ibm.com>
2019-02-10 17:52:06 +0530

    Added Production keys for facebook

--- config/keys_prod.js
+++ config/keys_prod.js
@@ -1,5 +1,7 @@
 module.exports = {
   mongoURI: process.env.MONGOLAB_COPPER_URI,
   googleClientID: process.env.GOOGLE_CLIENT_ID,
-  googleClientSecret: process.env.GOOGLE_CLIENT_SECRET
+  googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
+  FACEBOOK_APP_ID: process.env.FACEBOOK_APP_ID,
+  FACEBOOK_APP_SECRET: process.env.FACEBOOK_APP_SECRET
 }
